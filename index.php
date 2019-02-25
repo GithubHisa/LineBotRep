248bc4a6f3e8ba8a5f518d1766b5d7a8f58ae09d
GithubHisa <hisaii0904@gmail.com>
2019-02-25 15:58:30 +0900

    update 2019.02.25_7

--- index.php
+++ index.php
@@ -10,7 +10,9 @@ $json_object = json_decode($json_string);
 $replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
 $message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
 $message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
- 
+
+$sendType = 0;
+
 //メッセージタイプが「text」以外のときは何も返さず終了
 if($message_type != "text") exit;
 
@@ -23,12 +25,14 @@ else{
     $return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
 }
 
+$sendType = 1;
+
 //返信実行
 sending_messages($accessToken, $replyToken, $message_type, $return_message_text, $sendType);
 ?>
 <?php
 //メッセージの送信
-function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
+function sending_messages($accessToken, $replyToken, $message_type, $return_message_text, $sendType){
     //---
     // 確認ダイアログタイプ
     $send_format_gialog = [
@@ -67,8 +71,7 @@ function sending_messages($accessToken, $replyToken, $message_type, $return_mess
         "replyToken" => $replyToken,
         "messages" => [$response_format_text]
     ];
- 
-    
+     
     //curl実行
     $ch = curl_init("https://api.line.me/v2/bot/message/reply");
     curl_setopt($ch, CURLOPT_POST, true);
