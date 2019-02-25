4c62729593bb91fc94a14a75ce6afa1ebf7bf6c3
GithubHisa <hisaii0904@gmail.com>
2019-02-25 15:32:48 +0900

    update 2019.02.25_6

--- index.php
+++ index.php
@@ -16,14 +16,11 @@ if($message_type != "text") exit;
 
 
 //返信メッセージ
-$sendType = 0
 if($message_text == "てーば"){
     $return_message_text = "「てばさき」じゃねーよｗｗｗ";
-    $sendType = 1;
 }
 else{
     $return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
-    $sendType = 2;
 }
 
 //返信実行
